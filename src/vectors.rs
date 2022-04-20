e4c4cc36523cf0df9f7df60671f9bb116e39d333
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2022-04-20 18:53:12 -0300

    Creates vectors

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,3 +1,19 @@
+//Vectors are resizable arrays
+
 pub fn run(){
+    let numbers: Vec<i32> = vec![1,2,3,4,5];
+    println!("{:?}", numbers);
     
-}
+    // Get single vakue
+    println!("Single value: {}", numbers[0]);
+    
+    // Get vector lenght
+    println!("Vector lenght: {}", numbers.len());
+   
+    //Vectors are stack allocated
+    println!("This vector takes {} bytes in mem", std::mem::size_of_val(&numbers));
+   
+    //Get slice
+    let slice: &[i32] = &numbers[1..3];
+    println!("Slice: {:?}",slice);
+   }
