ff7723144db8da54611e8d80b895d17c507f910e
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2022-04-27 18:43:27 -0300

    Iterates on loops

--- src/loops.rs
+++ src/loops.rs
@@ -1,11 +1,25 @@
 // Iterate until a condition is met
 
 pub fn run(){
-    let mut count = 0;
+    let mut count = 1;
     
     // Infinite loop
-    loop {
-        count += 1;
-        println!("Number: {}", count);
+    // loop {
+    //     count += 1;
+    //     println!("Number: {}", count);
+    // }
+
+    while count<=100{
+        if count % 15 == 0 { println!("FizzBuzz")}
+        else if count % 5 == 0 {println!("Buzz")}
+        else if count % 3 == 0 {println!("Fizz")}
+        else {println!("{}", count)}
+        //increment
+        count += 1
     }
+
+
+
+
+
 }
